ea4a34bd9bf63a47113ee3c7ef197b67bdbc6180
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2025-02-28 00:11:46 +0330

    refactor: rename variables for clarity in main.py and update .gitignore

--- src/main.py
+++ src/main.py
@@ -52,30 +52,30 @@ screen_height = root.winfo_screenheight()
 timeframe = Frame(root, width=screen_width, height=screen_height, bg="Black")
 timeframe.grid(row=0, column=0)
 
-tkintertime = StringVar()
-timelabel = Label(
+tkinter_time = StringVar()
+time_label = Label(
     timeframe,
-    textvariable=tkintertime,
+    textvariable=tkinter_time,
     fg="#808080",
     bg="Black",
     font=("NovaMono", 150),
 )
-timelabel.place(y=screen_height / 2 - 60, x=screen_width / 2, anchor="center")
+time_label.place(y=screen_height / 2 - 60, x=screen_width / 2, anchor="center")
 
-tkinterdate = StringVar()
-datelabel = Label(
+tkinter_date = StringVar()
+date_label = Label(
     timeframe,
-    textvariable=tkinterdate,
+    textvariable=tkinter_date,
     fg="#808080",
     bg="Black",
     font=("Bahnschrift", 30),
 )
-datelabel.place(y=screen_height / 2 + 60, x=screen_width / 2, anchor="center")
+date_label.place(y=screen_height / 2 + 60, x=screen_width / 2, anchor="center")
 
 if __name__ == "__main__":
     while running:
-        tkintertime.set(value=strftime("%H:%M:%S"))
-        tkinterdate.set(value=strftime("%A, %e %B"))
+        tkinter_time.set(value=strftime("%H:%M:%S"))
+        tkinter_date.set(value=strftime("%A, %e %B"))
         root.update_idletasks()
         root.update()
         sleep(1)
